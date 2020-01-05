b0061ea2d212f1a7e40c69082716b20c2cac356c
Nick Hlushpenko <nick.hlushpenko@gmail.com>
2020-01-05 18:47:29 +0200

    Add message components Refactor CSS

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Profile from "./components/Profile/Profile";
+// import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs"
 
 const App = () => {
     return (
         <div className="app-wrapper">
             <Header/>
             <Nav/>
-            <Profile/>
+
+            <div className='app-wrapper-content'>
+                {/*<Profile/>*/}
+                <Dialogs />
+            </div>
+
         </div>
     );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import Posts from "./Posts/Posts";
 
 const Content = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
