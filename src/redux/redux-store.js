09e3f3923d0fa4e1508f610b20ebd884a05f0a6b
Nick Hlushpenko <nick.hlushpenko@gmail.com>
2020-02-18 14:58:50 +0200

    cookie, login, auth/me/

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./reducers/profile-reducer";
 import messagesReducer from "./reducers/messages-reducer";
 import usersReducer from "./reducers/users-reducer";
+import authReducer from "./reducers/auth-reducer";
 
 let reducers = combineReducers({
     profilePage : profileReducer,
     messagesPage: messagesReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store  = createStore(reducers);
